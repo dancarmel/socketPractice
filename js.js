41a6a970694918026fdc08a2780b5b8e67fa9a47
oren carmel <orenkafu@gmail.com>
2018-12-01 21:38:19 +0200

    oren comments

--- js.js
+++ js.js
@@ -1,3 +1,12 @@
+/*
+===================================
+            ASSIGNMENT
+===================================
+1. remove all useless comments  (old code, obvious comments)
+2. remove all useless console(log)s - the more you log, the less your logs mean.
+
+*/
+
 // import io from 'socket.io-client';
 console.log('gelo')
 const spotIMServer = "https://spotim-demo-chat-server.herokuapp.com"
@@ -12,40 +21,40 @@ const socket = io(spotIMChannel);
 
 //USER OBJECT WITH USER PROPERTIES
 user = {
-    userName:'hi',
-    password:'he',
-    imgUrl:'https://new.com'
-    
+    userName: 'hi',
+    password: 'he',
+    imgUrl: 'https://new.com'
+
 }
 
 let locStorage = window.localStorage;
 
 // function that overrides the user object values
-function saveUser(name,pass,img){
+function saveUser(name, pass, img) {
     user.userName = name;
     user.password = pass;
     user.imgUrl = img;
-} 
+}
 //authentication
-function authenticate(){
+function authenticate() {
     return true
 }
 
 // form submittion event will save all input values in local storage and pass them to user object
-document.addEventListener("submit", ()=>{    
+document.addEventListener("submit", () => {
     //get all input elements
     const inputs = document.getElementsByTagName("input");
     //authenticate
     authenticate();
     //save each input to the corresponding property
-    saveUser(inputs[0].value,inputs[1].value,inputs[2].value);
+    saveUser(inputs[0].value, inputs[1].value, inputs[2].value);
     //save all input user data to localstorage for future use
     locStorage.setItem("username", (user.userName));
     locStorage.setItem("isAuthenticated", (true));
     locStorage.setItem("imgUrl", (user.imgUrl));
-    
+
     //hide form
     const form = document.getElementById("login");
-    form.hidden = true  
+    form.hidden = true
 
 })
