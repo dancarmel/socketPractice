edea846c44b905d4e4f446eb8d076a0fa04906db
Dan Carmel <dan.carmel.mail@gmail.com>
2018-12-01 11:06:26 +0200

    First commit

---
+++ js.js
@@ -0,0 +1,9 @@
+// import io from 'socket.io-client';
+console.log('gelo')
+const spotIMServer = "https://spotim-demo-chat-server.herokuapp.com"
+const spotIMChannel = (`${spotIMServer}/chat`)
+console.log(`connecting to ${spotIMChannel}`)
+// const socket = io(spotIMChannel)(console.log(`connected to ${spotIMChannel}`));
+const socket = io(spotIMChannel);
+console.log(socket)
+socket.emit("hello");
