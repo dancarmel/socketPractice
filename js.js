23442f528f656b40ea7db67aabf958a7550f8bbc
Dan Carmel <dan.carmel.mail@gmail.com>
2018-12-01 12:38:10 +0200

    Changes to be committed: 	modified:   Index.html modified:   js.js

--- js.js
+++ js.js
@@ -6,8 +6,31 @@ console.log(`connecting to ${spotIMChannel}`)
 // const socket = io(spotIMChannel)(console.log(`connected to ${spotIMChannel}`));
 const socket = io(spotIMChannel);
 
-console.log(socket)
-socket.emit("hello world",(data) => {
-    console.log(data); 
-  });
-// socket.on("")
+user = {
+    userName:'hi',
+    password:'he',
+    imgUrl:'https://new.com'
+    
+}
+
+function saveUser(userName,password,imgUrl){
+    userName = user.userName;
+    password = user.password;
+    imgUrl = user.imgUrl;
+    console.log(user)
+
+} 
+
+
+document.addEventListener("submit", ()=>{
+    console.log("hi");
+    const form = document.getElementById("login");
+    const inputs = document.getElementsByTagName("input");
+    userArray = []
+    for(i in inputs){
+        userArray.push(i);
+    }
+    saveUser(userArray[0],userArray[1], userArray[2]);
+    console.log(user)
+    form.hidden = true  
+})
